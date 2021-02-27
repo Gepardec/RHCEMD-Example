b83c6d45d247e6f9c001bafb71c5ccd8570d901f
cchet <thomas.herzog@gepardec.com>
2021-02-27 16:05:57 +0100

    - Added doc to health part

--- src/main/java/at/gepardec/example/rhcemd/mp/health/RhcemdLivenessCheck.java
+++ src/main/java/at/gepardec/example/rhcemd/mp/health/RhcemdLivenessCheck.java
@@ -7,12 +7,17 @@ import org.eclipse.microprofile.health.Liveness;
 import javax.enterprise.context.ApplicationScoped;
 
 /**
- * This is the liveness health check.
+ * This is the liveness health check, determining if the service is alive.
+ * If this check fails the service will killed and restarted.
+ * This is the difference between an readiness and liveness check.
  */
 @Liveness
 @ApplicationScoped
 public class RhcemdLivenessCheck implements HealthCheck {
 
+    /**
+     * In case of an exception, the health implementation will return an Http-Status: 503, as specified
+     */
     @Override
     public HealthCheckResponse call() {
         return HealthCheckResponse.builder()
--- src/main/java/at/gepardec/example/rhcemd/mp/health/RhcemdReadinessCheck.java
+++ src/main/java/at/gepardec/example/rhcemd/mp/health/RhcemdReadinessCheck.java
@@ -7,18 +7,22 @@ import org.eclipse.microprofile.health.Readiness;
 import javax.enterprise.context.ApplicationScoped;
 
 /**
- * This is the readiness health check.
+ * This is the readiness health check, determining if the service is ready to accept connections.
+ * If this check fails the service will not get any request to serve
  */
 @Readiness
 @ApplicationScoped
 public class RhcemdReadinessCheck implements HealthCheck {
 
+    /**
+     * In case of an exception, the health implementation will return an Http-Status: 503, as specified
+     */
     @Override
     public HealthCheckResponse call() {
         return HealthCheckResponse.builder()
-                .name("RhcemdReadinessCheck")
-                .state(true)
-                .withData("checks", "none")
+                .up()
+                .name("RhcemdReadinessCheck") // A name a consumer can use
+                .withData("checks", "none") // Key-value pairs for the data part fo the response
                 .build();
     }
 }
